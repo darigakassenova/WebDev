56b4ec0a61a9f35078abc633730cc30ebd177d73
Dariga <dariga@MacBook-Air-Dariga.local>
2023-02-16 18:24:45 +0600

    added JS->add new tasks; update css and html

---
+++ lab3/Task2/todo.js
@@ -0,0 +1,24 @@
+let toDoList = [];
+
+document.querySelector('.add-tasks').onclick = function(){
+
+  if(document.querySelector('.input-tasks').value.length == 0){
+     alert('the field cannot be empty!');
+  }
+
+  else{
+     document.querySelector('.task-list').innerHTML += `
+     <div class="task">
+        <button class="done" onclick = completed(this)><i class=check></i></button>
+        <span>
+           ${document.querySelector('.input-tasks').value}
+        </span> 
+        <button class="remove" onclick = deleting()>delete</button>
+     </div>`;
+     toDoList.push(document.querySelector('.input-tasks').value);
+     console.log(toDoList);
+
+     document.querySelector('.input-tasks').value = '';
+  }
+
+}
