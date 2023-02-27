cc73ed5929709b567029014634d90901f6173cd4
Dariga <dariga@MacBook-Air-Dariga.local>
2023-02-27 10:34:06 +0600

    added categories

---
+++ lab5/task/src/app/categories.ts
@@ -0,0 +1,27 @@
+export interface Category {
+    id: number;
+    name: string;
+    img: string;
+  }
+  export const categories = [
+    {
+      id: 1,
+      name: 'Books',
+      img: 'https://simg.marwin.kz/media/books/hud-class-min.png'
+    },
+    {
+        id: 2,
+        name: 'Notebooks',
+        img: 'https://simg.marwin.kz/media/catalog/product/2/5/bloknot_a5_135h213_96_l_7b_joy_book_nochnoe_nebo.jpg'
+      },
+    {
+        id: 3,
+        name: 'Sweets',
+        img: 'https://simg.marwin.kz/media/catalog/product/1/2/marmelad_jevatelnyy_fini_kola_butylochka_v_sahare90gr_ispaniya.jpg'
+      },
+    {
+        id: 4,
+        name: 'Toys',
+        img: 'https://freepngimg.com/thumb/categories/2432.png'
+      },
+    ]
---
+++ lab5/task/src/app/product-categories/product-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { categories } from '../categories';
+
+@Component({
+  selector: 'app-product-categories',
+  templateUrl: './product-categories.component.html',
+  styleUrls: ['./product-categories.component.css']
+})
+export class ProductCategoriesComponent {
+  categories = [...categories];
+}
